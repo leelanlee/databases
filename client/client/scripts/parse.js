f08ede7998bc30174c8fd00c1270ad15dc05b3bc
andy-a-d-nguyen <andy.a.d.nguyen@zoho.com>
2020-10-29 14:45:11 -0700

    Troubleshooting request object

--- client/client/scripts/parse.js
+++ client/client/scripts/parse.js
@@ -3,17 +3,7 @@ var Parse = {
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function(message, successCB, errorCB = null) {
-
-    $.ajax({
-      url: Parse.server,
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: successCB,
-      error: errorCB || function (error) {
-        console.error('chatterbox: Failed to create message', error);
-      }
-    });
+    // todo: save a message to the server
   },
 
   readAll: function(successCB, errorCB = null) {

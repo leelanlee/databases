0880678a0b8351ae27e1f984b2a1901c339f3ea5
andy-a-d-nguyen <andy.a.d.nguyen@zoho.com>
2020-10-28 20:39:05 -0700

    implemented chat database

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  username TEXT
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT PRIMARY KEY,
+  roomname TEXT
+);
 
-
-
+CREATE TABLE messages (
+  id INT PRIMARY KEY,
+  message TEXT,
+  created_at DATE,
+  room_id INT,
+  user_id INT,
+  FOREIGN KEY (room_id) REFERENCES rooms(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
